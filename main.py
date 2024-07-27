aa9dd7a814284b78bb478b8df713ec8783fe6b53
blink-blade <micah@blinkblade.net>
2024-07-27 17:41:00 -0400

    Wall rect not working because only one iteration.

--- main.py
+++ main.py
@@ -22,27 +22,8 @@ class Rect(Image):
         self.x_velocity = 0
         self.y_velocity = 0
         super().__init__(*args, **kwargs)
-        self.instructions = []
         self.size_hint = None, None
 
-    def remove_extra_instructions(self):
-        for instruction in self.instructions:
-            self.canvas.remove(instruction)
-        self.instructions.clear()
-
-    def add_instructions(self):
-        for rect in rects:
-            if rect == self:
-                continue
-            intersection = get_intersection_of_rects(rect, self)
-            if intersection[2] * intersection[3] > 0:
-                color = Color(1, 0.5, 1, 1)
-                brightener_instruction = Rectangle()
-                self.instructions.append(brightener_instruction)
-                brightener_instruction.pos = intersection[0], intersection[1]
-                brightener_instruction.size = intersection[2], intersection[3]
-                self.canvas.add(color)
-                self.canvas.add(brightener_instruction)
 
     def solve_with_rect(self, rect):
         intersection_size = get_intersection_of_rects(rect, self)[2:]
@@ -93,9 +74,7 @@ class Rect(Image):
         if not self.static:
             self.x += self.x_velocity
             self.y += self.y_velocity
-        self.solve_collisions()
-        self.remove_extra_instructions()
-        self.add_instructions()
+            self.solve_collisions()
 
 
 def get_intersection_of_rects(rect_one, rect_two):
@@ -122,51 +101,31 @@ def get_intersection_of_rects(rect_one, rect_two):
 
 class GameApp(App):
     def __init__(self, **kwargs):
-        self.selected_rect = None
-        self.previous_touch_position = None
         super(GameApp, self).__init__(**kwargs)
         # Window.bind(on_motion=on_motion, on_resize=self.on_resize)
         Window.bind(on_key_up=on_key_up, on_key_down=on_key_down)
 
-    def on_touch_down(self, touch):
-        shortest_distance = 10000
-        closest_rect = None
-        for rect in rects:
-            if rect.collide_point(touch.x, touch.y):
-                if get_distance_between_points((rect.x, rect.y), (touch.x, touch.y)) < shortest_distance:
-                    closest_rect = rect
-        self.selected_rect = closest_rect
-        self.previous_touch_position = touch.pos
-
-    def on_touch_move(self, touch):
-        if not self.selected_rect:
-            return
-        self.selected_rect.x += touch.x - self.previous_touch_position[0]
-        self.selected_rect.y += touch.y - self.previous_touch_position[1]
-        self.previous_touch_position = touch.pos
-        # This makes things look cleaner when moving the rectangles arouned
-        self.update(0)
-
-    def on_touch_up(self, touch):
-        self.selected_rect = None
-
     def on_start(self):
-        self.root.on_touch_down = self.on_touch_down
-        self.root.on_touch_move = self.on_touch_move
-        self.root.on_touch_up = self.on_touch_up
         Clock.schedule_interval(self.update, 1 / 60)
         self.player_rect = Rect(pos=(500, 500), size=(200, 200), color=(1, 1, 0, 1), static=False)
         self.root.add_widget(self.player_rect)
         self.ground_rect = Rect(pos=(0, 0), size=(1920, 200), color=(1, 0, 0.1, 1), static=True)
         self.root.add_widget(self.ground_rect)
+        self.wall_rect = Rect(pos=(0, 200), size=(200, 1080), color=(1, 0.5, 0.1, 1), static=True)
+        self.root.add_widget(self.wall_rect)
+
 
     def update(self, dt):
         for rect in rects:
             rect.update(dt)
 
-        if self.selected_rect:
-            self.selected_rect.x_velocity = 0
-            self.selected_rect.y_velocity = 0
+        if controls.get('space'):
+            self.player_rect.y_velocity += 10
+            controls.pop('space')
+        if controls.get('a'):
+            self.player_rect.x_velocity = -5
+        if controls.get('d'):
+            self.player_rect.x_velocity = 5
 
 if __name__ == '__main__':
     GameApp().run()
